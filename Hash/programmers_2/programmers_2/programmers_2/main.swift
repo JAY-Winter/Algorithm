6011be98f6f4dd803709193ea0bae51a8a2ee990
JAY-Winter <rubat0@naver.com>
2022-05-20 17:23:25 +0900

    refactor: 변수 위치 및 블랭크 수정

--- Hash/programmers_2/programmers_2/programmers_2/main.swift
+++ Hash/programmers_2/programmers_2/programmers_2/main.swift
@@ -8,20 +8,19 @@
 import Foundation
 
 func solution(_ clothes:[[String]]) -> Int {
-    let clothesInfo = arrangeClothes(clothes)
-    print(clothesInfo)
-    
     var result: Int = 0
+    let clothesInfo = arrangeClothes(clothes)
     let categoryCount: Int = clothesInfo.values.count
     
-    if categoryCount >= 2{
+    
+    if categoryCount >= 2 {
         for cloth in clothesInfo.values {
             result += cloth.count
             result += 1
         }
     } else {
-        for i in clothesInfo.values {
-            result += i.count
+        for cloth in clothesInfo.values {
+            result += cloth.count
         }
     }
     print("result: \(result)")
@@ -31,7 +30,6 @@ func solution(_ clothes:[[String]]) -> Int {
 
 func arrangeClothes(_ clothes: [[String]]) -> [String: [String]] {
     var clothesInfo: [String: [String]] = [:]
-    
     let category = clothes.map {$0[1] }
     let cloth = clothes.map {$0[0] }
     
