22e420a9e488f1e10bd123c4e5eecb4332ebedb6
JAY-Winter <rubat0@naver.com>
2023-09-28 20:22:41 +0900

    백준 16236 아기 상어

--- BAEKJOON/16236.py
+++ BAEKJOON/16236.py
@@ -1,130 +1,127 @@
-# 최초 아기 상어 크기 2
-# 상어는 1초에 상하좌우 인접한 한 칸씩 이동
+'''
+Q. 아기 상어가 엄마 사엉에게 도움을 요청하지 않고 물고기를 잡아먹을 수 있는 시간을 출력하라
 
-# 자신의 크기보다 큰 물고기가 있는 칸은 지나갈 수 없다
-# 자신의 크기보다 작은 물고기만 먹을 수 있다
-# 크기가 같은 물고기는 먹을 수 없지만 지나갈 수 있다
-# 물고기를 먹으면 그 칸은 빈 칸이 된다
-# 물고기를 먹으면 크기가 1 씩 증가한다
+- 가장 처음에 상어의 크기는 2
+- 자신의 크기와 같은 수의 물고리를 먹을 때 마다 크기가 1씩 증가
+    크기가 2일 때는 2마리 먹어야 1 증가
+- 1초에 상하좌우로 이동
 
-# 더 이상 먹을 수 있는 물고기가 공간에 없다면 도움 요청
-# 먹을 수 있는 물고기가 1 마리라면 그 물고기를 먹음
-# 먹을 수 있는 물고기가 1 마리 보다 많다면, 거리가 가장 가까운 물고기를 먹음
-## 거리는 최솟값
-## 거리가 가까운 물고기가 많다면, 가장 위에 있는 물고기, 그러한 물고기가 여러마리라면, 가장 왼쪽에 있는 물고기
+- 지나갈 수 있는 조건
+    1. 자신의 크기보다 작아야함
+    2. 자신의 크기와 같아야함
 
-# 출력
-# 몇 초 동안 도움을 요청하지 않고 물고기를 잡아먹을 수 있는 지 구하라
+- 먹을 수 있는 조건
+    1. 자신의 크기보다 작은 물고기만 가능
 
-# 0: 빈칸
-# 1 ~ 6 : 물고기 크기
-# 9 : 아기상어위치
+- 이동 결정 방법
+    - 더 이상 먹을 수 있는 물고기가 공간에 없다면 엄마에게 도움 요청
+    - 먹을 수 있는 물고기가 1마리라면, 그 물고기를 먹음
+    - 먹을 수 있는 물고기가 1마리보다 많다면, 거리가 가장 가까운 물고기
+        - 거리가 가까운 물고기가 많다면, 가장 위에 있는 물고기
+        - 가장 왼쪽에 있는 물고기
 
-"""
-1. 전체 칸을 순회하며 먹을 수 있는 물고기가 있는지 확인
-2-1. 먹을 수 있는 물고기가 있을 때
-3-1. 먹을 수 있는 물고기가 1마리일 때
-3-2. 먹을 수 있는 물고기가 1마리 이상일 때
-2-2. 먹을 수 있는 물고기가 없을 때 : 종료
+- 칸
+    - 0 : 빈 칸
+    - 1 ~ 6 : 물고기의 크기
+    - 9 : 상어의 위치
+    - N <= 20
 
-별도의 함수: 현재 상어 위치에서 먹을 수 있는 물고기의 위치 거리(지나야 하는 칸의 개수) 구하기
+Sol)
+1. 아기 상어의 위치를 찾는다
+2. 현재 위치에서 먹을 수 있는 상어를 찾는다
+    - 이동 결정 방법 참고
+3. 먹을 수 있는 상어가 있을 때, 상어를 먹는다
+    이때, 크기가 M 이면 M 마리를 먹어야 크기가 1 증가함
+'''
 
-"""
+from collections import deque
 
 
-N = int(input())
-arr = [list(map(int, input().split())) for _ in range(N)]
-size = 2
-sec = 0
-
-def can_eat(size):
-    for i in range(N):
-        for j in range(N):
-            if arr[i][j] != 0 and size > arr[i][j]:
-                return True
-    return False
-
-def how_many_can_eat(size):
-    cnt = 0
-    pos_list = []
-    for i in range(N):
-        for j in range(N):
-            if arr[i][j] != 0 and size > arr[i][j]:
-                cnt += 1
-                new_pos = (i, j)
-                pos_list.append(new_pos)
-    return (cnt, pos_list)
-
-def cal_distance(now_pos, search_pos_list):
-    print('cal_distance',now_pos, search_pos_list)
-    dr = [-1, 1, 0, 0]
-    dc = [0, 0, -1, 1]
-    distance_list = []
-
-    for search_pos in search_pos_list:
-        r, c = now_pos
-        distance = 0
-        Q = [(r, c, distance)]
-        visited = [[0] * N for _ in range(N)]
-        while Q:
-            r, c, distance = Q.pop(0)
-            visited[r][c] = 1
-            now_pos = (r, c)
-
-            if now_pos == search_pos:
-                break
-            
-            for d in range(4):
-                new_r = r + dr[d]
-                new_c = c + dc[d]
-
-                if new_r >= N or new_r < 0 or new_c >= N or new_c < 0:
-                    continue
-                if visited[new_r][new_c]:
-                    continue
-                if arr[new_r][new_c] > size:
-                    continue
-                
-                Q.append((new_r, new_c, distance+1))
-
-        distance_list.append((distance, search_pos))
-    return distance_list
-
-def check_start_pos():
+def find_baby_shark_position():
     for i in range(N):
         for j in range(N):
             if arr[i][j] == 9:
-                return (i, j)
+                return i, j
 
-def eat(pos):
-    r, c = pos
+
+def calculate_distance(r, c, size):
+    global N
+    dr = [-1, 1, 0, 0]
+    dc = [0, 0, -1, 1]
+    distance_arr = [[-1] * N for _ in range(N)]
+    distance_arr[r][c] = 0
+    Q = deque([(r, c)])
+    while Q:
+        r, c = Q.popleft()
+        for d in range(4):
+            nr, nc = r + dr[d], c + dc[d]
+            # 좌표 내 & 아직 방문하지 않은 곳 & 현재 사이즈보다 작거나 같은 곳
+            if N > nr >= 0 and N > nc >= 0 and distance_arr[nr][nc] == -1 and size >= arr[nr][nc]:
+                distance_arr[nr][nc] = distance_arr[r][c] + 1
+                Q.append((nr, nc))
+    return distance_arr
+
+
+# 먹을 수 있는 상어 위치와 거리를 반환
+def find_can_eat_every_shark(size, distance_arr):
+    which_can_ate = []
+    for i in range(N):
+        for j in range(N):
+            if arr[i][j] < size and arr[i][j] != 0:
+                distance = distance_arr[i][j]
+                if distance != -1:
+                    which_can_ate.append((i, j, distance))
+    return which_can_ate
+
+
+def find_can_eat_specific_shark(which_can_ate):
+    if len(which_can_ate) == 1:
+        specific_shark = which_can_ate[0]
+    else:
+        # 거리가 가장 가까운 물고기 > 가장 위에 있는 물고기 > 가장 왼쪽에 있는 물고기
+        specific_shark = sorted(which_can_ate, key=lambda x: (x[2], x[0], x[1]))[0]
+    return specific_shark
+
+
+def eat_shark(r, c):
     arr[r][c] = 0
     return
 
 
-# main 실행
-start_pos = check_start_pos()
-while can_eat(size):
-    can_eat_cnt, can_eat_pos_list = how_many_can_eat(size)
+def main():
+    cur_size = 2
+    cur_eat_cnt = 0
+    total_sec = 0
+    init_shark_r, init_shark_c = find_baby_shark_position()
+    arr[init_shark_r][init_shark_c] = 0
+    while True:
+        # 현재 상어의 사이즈와 위치를 기반으로 지나갈 수 있는 모든 좌표에 대한 거리를 포함한 리스트
+        distance_arr = calculate_distance(init_shark_r, init_shark_c, cur_size)
+        # 현재 상어의 사이즈를 기반으로 먹을 수 있는 모든 상어의 좌표와 거리를 포함한 리스트
+        which_can_eat = find_can_eat_every_shark(cur_size, distance_arr)
 
-    if can_eat_cnt == 1:
-        distance = cal_distance(start_pos, can_eat_pos_list)
-        print('distance', distance)
-        eat_pos = can_eat_pos_list[0][1]
-        r = eat_pos[0]
-        c = eat_pos[1]
-        arr[r][c] = 0
-        size += 1
-        print(size)
-    elif can_eat_cnt > 1:
-        print('한 마리 이상 먹을 수 있음')
-        distance = cal_distance(start_pos, can_eat_pos_list)
-        print('distance', distance)
-        sorted_distance = sorted(distance, key=lambda x: (x[0], x[1][0], x[1][1], print(x[1][0], x[1][1])))
-        print('sorted_distance', sorted_distance)
-        eat_pos = sorted_distance[0][1]
-        print('eat_pos', eat_pos)
-        r = eat_pos[0]
-        c = eat_pos[1]
-        arr[r][c] = 0
-        size += 1
+        # 더 이상 먹을 수 있는 물고기가 공간에 없다는 뜻
+        if not which_can_eat:
+            return total_sec
+        # 먹을 수 있는 모든 상어 중 "이동결정방법"에 따라 분류된 한 마리의 상어
+        specific_shark = find_can_eat_specific_shark(which_can_eat)
+        # 한 마리의 상어를 먹음
+
+        eat_shark(specific_shark[0], specific_shark[1])
+        cur_eat_cnt += 1
+
+        total_sec += specific_shark[2]
+
+        init_shark_r, init_shark_c = specific_shark[0], specific_shark[1]
+        # 지금까지 먹었던 상어의 마리 수가 현재 사이즈와 같을 시
+        if cur_eat_cnt == cur_size:
+            cur_size += 1
+            cur_eat_cnt = 0
+
+
+N = int(input())
+arr = [list(map(int, input().split())) for _ in range(N)]
+
+answer = main()
+print(answer)
+
