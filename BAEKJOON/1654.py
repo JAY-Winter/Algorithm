6a399f36343c1087cfcca65b6bac6096ff9e4ad3
JAY-Winter <rubat0@naver.com>
2023-10-21 22:27:55 +0900

    백준 1654 랜선 자르기

--- BAEKJOON/1654.py
+++ BAEKJOON/1654.py
@@ -1,3 +1,12 @@
+'''
+갖고있는 K 개의 랜선은 길이가 제각각이다
+랜선을 모두 N 개의 같은 길이의 랜선으로 만들고 싶었기 때문에
+K개의 랜서을 잘라서 만들어야한다
+
+Q) 최대 랜선의 길이를 구하라
+
+'''
+
 # 만들 수 있는 최대 랜선의 길이를 구하라
 
 # K : 갖고있는 랜선의 개수 <= 1만
@@ -6,4 +15,18 @@
 K, M = map(int, input().split())
 
 lans = [int(input()) for _ in range(K)]
-print(sorted(lans))
+
+
+start, end = 1, max(lans)
+
+while start <= end:
+    mid = (start + end) // 2
+
+    total = sum([cable // mid for cable in lans])
+
+    if (total >= M):
+        start = mid + 1
+    else:
+        end = mid - 1
+
+print(end)
